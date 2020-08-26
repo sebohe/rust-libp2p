0a5ecacd9a22343da6b304194baa0f6e8a99a9b7
Kirk Baird <baird.k@outlook.com>
2020-08-26 12:43:55 +1000

    Add extra fuzz targets
    Signed-off-by: Kirk Baird <baird.k@outlook.com>

---
+++ protocols/gossipsub/fuzz/fuzz_targets/fuzz_gossipsub_decode_permissive.rs
@@ -0,0 +1,22 @@
+#![no_main]
+use bytes::BytesMut;
+use futures_codec::{Decoder, Encoder};
+use libfuzzer_sys::fuzz_target;
+use libp2p_gossipsub::{protocol::GossipsubCodec, HandlerEvent, ValidationMode};
+use unsigned_varint::codec;
+
+fuzz_target!(|data: &[u8]| {
+    let mut codec = GossipsubCodec::new(codec::UviBytes::default(), ValidationMode::Permissive);
+    let mut buf: BytesMut = data.into();
+    if let Ok(rpc) = codec.decode(&mut buf) {
+        if let Some(rpc) = rpc {
+            match rpc {
+                HandlerEvent::Message { rpc, .. } => {
+                    let mut buf = BytesMut::new();
+                    codec.encode(rpc, &mut buf);
+                }
+                _ => {}
+            }
+        }
+    }
+});
